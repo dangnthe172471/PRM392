fdaf31b218e4e26cc66831421666de53f978d5b2
dangnthe172471 <169129054+dangnthe172471@users.noreply.github.com>
2025-07-13 23:11:17 +0700

    update baseactivity

--- Android/app/src/main/java/com/example/prm392/BlogActivity.java
+++ Android/app/src/main/java/com/example/prm392/BlogActivity.java
@@ -9,7 +9,6 @@ import com.example.prm392.R;
 
 
 import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -17,35 +16,36 @@ import com.example.prm392.Retrofit.AccountManager;
 import com.example.prm392.adapter.BlogAdapter;
 import com.example.prm392.database.BlogRepository;
 import com.example.prm392.model.BlogModel;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class BlogActivity extends AppCompatActivity {
+public class BlogActivity extends BaseActivity {
 
     private RecyclerView recyclerBlogs;
     private BlogAdapter blogAdapter;
     private EditText etSearchBlog;
-    private BottomNavigationView bottomNavigationView;
     private List<BlogModel> fullBlogList;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_blog);
+    protected int getLayoutResourceId() {
+        return R.layout.activity_blog;
+    }
 
+    @Override
+    protected void initViews() {
         // Ánh xạ view
         recyclerBlogs = findViewById(R.id.recyclerBlogs);
         etSearchBlog = findViewById(R.id.etSearchBlog);
-        bottomNavigationView = findViewById(R.id.bottomNavigation);
 
         recyclerBlogs.setLayoutManager(new LinearLayoutManager(this));
 
         // Load dữ liệu
         loadBlogData();
+    }
 
+    @Override
+    protected void setupListeners() {
         // Tìm kiếm realtime
         etSearchBlog.addTextChangedListener(new TextWatcher() {
             @Override
@@ -59,38 +59,11 @@ public class BlogActivity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s) { }
         });
+    }
 
-        // Xử lý click BottomNavigationView
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            int id = item.getItemId();
-
-            if (id == R.id.nav_home) {
-                startActivity(new Intent(BlogActivity.this, MainActivity.class));
-                overridePendingTransition(0,0);
-                finish(); // Đóng BlogActivity để tránh stack quá nhiều
-                return true;
-            } else if (id == R.id.nav_bookings) {
-                // Đã ở bookings (blog) rồi
-                return true;
-            } else if (id == R.id.nav_chat) {
-                // startActivity(new Intent(BlogActivity.this, ChatActivity.class));
-                return true;
-            } else if (id == R.id.nav_profile) {
-                int userId = AccountManager.getUserId(this);
-                if(userId != -1) {
-                    startActivity(new Intent(BlogActivity.this, ProfileActivity.class));
-                } else {
-                    startActivity(new Intent(BlogActivity.this, LoginActivity.class));
-                }
-                return true;
-            }
-
-            return false;
-        });
-
-
-        // Set mặc định tab Blog được chọn
-        bottomNavigationView.setSelectedItemId(R.id.nav_bookings);  // hoặc R.id.nav_blog nếu bạn thêm Blog vào menu
+    @Override
+    protected int getDefaultSelectedTab() {
+        return R.id.nav_chat;
     }
 
     private void loadBlogData() {
